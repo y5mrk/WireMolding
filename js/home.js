a46aeb04520d7b402d3c92ab0857c7a53c990cdb
ysmrk <kzm326.ag@gmail.com>
2017-03-08 19:32:31 +0900

    show svg

--- js/home.js
+++ js/home.js
@@ -69,7 +69,7 @@ $('#tab-menu a').on('show.bs.tab', function (event) {
 	var sampleTextRelatedTarget = $(event.relatedTarget).text();
 	if(sampleTextTarget == "Construct"){
     init();
-    animate();
+    // animate();
   }else{
     stop();
   }
@@ -103,10 +103,11 @@ function getValue(newVal){
 
 // three.js
 
-var scene, axes, grid, camera, controls, renderer;
+var scene, axes, grid, camera, orbit, webGLRenderer;
 var geometry, material, mesh;
 
 function init() {
+    var stats = initStats();
 
     scene = new THREE.Scene();
 
@@ -122,7 +123,8 @@ function init() {
     // camera.position.z = 1000;
     camera.position.set(30, 45, 1000);
     camera.lookAt(scene.position);
-    controls = new THREE.OrbitControls(camera);
+    // camera.lookAt(new THREE.Vector3(60, -60, 0));
+    orbit = new THREE.OrbitControls(camera);
 
     geometry = new THREE.BoxGeometry( 200, 200, 200 );
     material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
@@ -130,23 +132,97 @@ function init() {
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( 600, 400 );
-    renderer.setClearColor(0xF0F0F0, 1.0);
+    webGLRenderer = new THREE.WebGLRenderer();
+    webGLRenderer.setSize( 600, 400 );
+    webGLRenderer.setClearColor(0xF0F0F0, 1.0);
+    // webGLRenderer.shadowMapEnabled = true;
 
-    document.getElementById("viewer-area").appendChild( renderer.domElement );
+    var shape = createMesh(new THREE.ShapeGeometry(drawShape()));
+    // add the sphere to the scene
+    scene.add(shape);
 
-}
+    var spotLight = new THREE.DirectionalLight(0xffffff);
+    spotLight.position = new THREE.Vector3(70, 170, 70);
+    spotLight.intensity = 0.7;
+    spotLight.target = shape;
+    scene.add(spotLight);
 
-function animate() {
+    document.getElementById("viewer-area").appendChild(webGLRenderer.domElement);
 
-    controls.update();
-    animate_id = requestAnimationFrame( animate );
-
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
-
-    renderer.render( scene, camera );
+    var step = 0;
+    // setup the control gui
+    var controls = new function () {
+        this.amount = 2;
+        this.bevelThickness = 2;
+        this.bevelSize = 0.5;
+        this.bevelEnabled = true;
+        this.bevelSegments = 3;
+        this.bevelEnabled = true;
+        this.curveSegments = 12;
+        this.steps = 1;
+        this.asGeom = function () {
+            // remove the old plane
+            scene.remove(shape);
+            // create a new one
+            var options = {
+                amount: controls.amount,
+                bevelThickness: controls.bevelThickness,
+                bevelSize: controls.bevelSize,
+                bevelSegments: controls.bevelSegments,
+                bevelEnabled: controls.bevelEnabled,
+                curveSegments: controls.curveSegments,
+                steps: controls.steps
+            };
+            shape = createMesh(new THREE.ExtrudeGeometry(drawShape(), options));
+            // add it to the scene.
+            scene.add(shape);
+        };
+    };
+    var gui = new dat.GUI();
+    gui.add(controls, 'amount', 0, 20).onChange(controls.asGeom);
+    gui.add(controls, 'bevelThickness', 0, 10).onChange(controls.asGeom);
+    gui.add(controls, 'bevelSize', 0, 10).onChange(controls.asGeom);
+    gui.add(controls, 'bevelSegments', 0, 30).step(1).onChange(controls.asGeom);
+    gui.add(controls, 'bevelEnabled').onChange(controls.asGeom);
+    gui.add(controls, 'curveSegments', 1, 30).step(1).onChange(controls.asGeom);
+    gui.add(controls, 'steps', 1, 5).step(1).onChange(controls.asGeom);
+    controls.asGeom();
+    render();
+    function drawShape() {
+        var svgString = document.querySelector("#batman-path").getAttribute("d");
+        var shape = transformSVGPathExposed(svgString);
+        // return the shape
+        return shape;
+    }
+    function createMesh(geom) {
+        geom.applyMatrix(new THREE.Matrix4().makeTranslation(-390, -74, 0));
+        // assign two materials
+        var meshMaterial = new THREE.MeshPhongMaterial({color: 0x333333, shininess: 100, metal: true});
+        var mesh = new THREE.Mesh(geom, meshMaterial);
+        mesh.scale.x = 0.1;
+        mesh.scale.y = 0.1;
+        mesh.rotation.z = Math.PI;
+        mesh.rotation.x = -1.1;
+        return mesh;
+    }
+    function render() {
+        stats.update();
+        shape.rotation.y = step += 0.005;
+        orbit.update();
+        // render using requestAnimationFrame
+        animate_id = requestAnimationFrame(render);
+        webGLRenderer.render(scene, camera);
+    }
+    function initStats() {
+        var stats = new Stats();
+        stats.setMode(0); // 0: fps, 1: ms
+        // Align top-left
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+        document.getElementById("Stats-output").appendChild(stats.domElement);
+        return stats;
+    }
 
 }
 
@@ -159,7 +235,6 @@ function stop(){
   // sceen = null;
 
   cancelAnimationFrame( animate_id );
-  this.renderer.domElement.addEventListener('dblclick', null, false); //remove listener to render
   this.scene = null;
   this.projector = null;
   this.camera = null;
